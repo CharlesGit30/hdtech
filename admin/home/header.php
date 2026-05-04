356f18ae39e21d9b976132ffe11c95bcc5f50a2b
CharlesGit30 <charlesgit30@gmail.com>
2026-05-03 21:26:04 -0300

    Add files via upload

--- admin/home/header.php
+++ admin/home/header.php
@@ -1,1 +1,36 @@
 
+
+<header class="admin-header">
+    <div class="nav container">
+
+        <!-- LOGO -->
+        <a href="../home/home.php" class="logo">
+            HD <span>Tech</span>
+        </a>
+
+        <!-- NAV RIGHT -->
+        <div class="nav-right">
+
+            <?php if (isset($_SESSION['usuario_nome'])): ?>
+                <div class="user-box">
+                    <i class='bx bx-user'></i>
+                    <span>
+                        <?php echo htmlspecialchars($_SESSION['usuario_nome']); ?>
+                    </span>
+                </div>
+
+               
+
+            <?php else: ?>
+                <a href="../login/login.php">
+                    <i class='bx bx-user'></i>
+                </a>
+            <?php endif; ?>
+       
+            <i class='bx bx-menu' id="menu-icone"></i>
+
+        </div>
+
+        <a href="../login/logout.php" class="logout-btn">Sair</a>
+    </div>
+</header>
